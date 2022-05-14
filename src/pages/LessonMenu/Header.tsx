efc6a145233a69aeb5a01a7d415ab575a873d202
Yan Kahanov <kahanov.yan@gmail.com>
2022-05-14 13:38:30 +0500

    changed lessonMenu header

--- src/pages/LessonMenu/Header.tsx
+++ src/pages/LessonMenu/Header.tsx
@@ -1,5 +1,11 @@
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import {
+  AppBar,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import styled from "@emotion/styled";
@@ -7,18 +13,20 @@ import styled from "@emotion/styled";
 type Props = {};
 
 const LessonMenuHeader: React.FC<Props> = () => {
-  const navigate = useNavigate()
-  const {id} = useParams()
+  const navigate = useNavigate();
+  const { id } = useParams();
 
   return (
     <AppBar>
       <Toolbar>
+        <Container maxWidth='md'>
           <IconButton edge="start" size="large" onClick={() => navigate(-1)}>
             <ArrowBackIcon />
           </IconButton>
           <Title variant="h6">
             Урок {id}
           </Title>
+        </Container>
       </Toolbar>
     </AppBar>
   );
@@ -26,9 +34,9 @@ const LessonMenuHeader: React.FC<Props> = () => {
 
 const Title = styled(Typography)`
   position: absolute;
-  top: 50%;  
+  top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-`
+`;
 
 export default LessonMenuHeader;
