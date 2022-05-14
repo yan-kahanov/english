7488bb1bbce9d6ee3840f8ca035645959f9cf0e1
Yan Kahanov <kahanov.yan@gmail.com>
2022-05-14 13:33:50 +0500

    changed lessons header

--- src/pages/Lessons/Header.tsx
+++ src/pages/Lessons/Header.tsx
@@ -1,35 +1,43 @@
 import {
-    AppBar,
-    IconButton,
-    Stack,
-    Toolbar,
-    Typography,
-  } from "@mui/material";
-  import MenuIcon from "@mui/icons-material/Menu";
-  import React from "react";
-  
-  type Props = {};
-  
-  const LessonsHeader: React.FC<Props> = () => {
-    return (
-      <AppBar position="static">
-        <Toolbar>
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          sx={{flexGrow: 1}}
-        >
-          <Typography variant="h6" component="div">
-            Уроки
-          </Typography>
-          <IconButton size="large" edge="end" color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-        </Stack>
-        </Toolbar>
-      </AppBar>
-    );
-  };
-  
-  export default LessonsHeader;
+  AppBar,
+  IconButton,
+  Stack,
+  Toolbar,
+  Typography,
+  Container,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import React from "react";
+
+type Props = {};
+
+const LessonsHeader: React.FC<Props> = () => {
+  return (
+    <AppBar>
+      <Toolbar>
+        <Container maxWidth="md">
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{ flexGrow: 1 }}
+          >
+            <Typography variant="h6" component="div">
+              Уроки
+            </Typography>
+            <IconButton
+              size="large"
+              edge="end"
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+          </Stack>
+        </Container>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default LessonsHeader;
--- src/pages/Lessons/List.tsx
+++ src/pages/Lessons/List.tsx
@@ -2,13 +2,14 @@ import { List } from "@mui/material";
 import React from "react";
 import LessonsItem from "./Item";
 import lessons from '../../lessons.json'
+import styled from "@emotion/styled";
 
 type Props = {};
 
 const LessonsList: React.FC<Props> = () => {
 
   return (
-    <List>
+    <StyledList>
       {lessons.map(lesson => (
         <LessonsItem 
         key={lesson.id}
@@ -16,8 +17,12 @@ const LessonsList: React.FC<Props> = () => {
         description={lesson.description}
         />
       ))}
-    </List>
+    </StyledList>
   );
 };
+
+const StyledList = styled(List)`
+  margin-top: 75px;
+`
 
 export default LessonsList;
