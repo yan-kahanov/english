6253177eced6b77820d3664731f415cff3b3cf2d
Yan Kahanov <kahanov.yan@gmail.com>
2022-06-21 15:20:40 +0500

    removed MenuIcon

--- src/pages/Lessons/Header.tsx
+++ src/pages/Lessons/Header.tsx
@@ -1,12 +1,10 @@
 import {
   AppBar,
-  IconButton,
   Stack,
   Toolbar,
   Typography,
   Container,
 } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
 import React from "react";
 
 type Props = {};
@@ -25,14 +23,6 @@ const LessonsHeader: React.FC<Props> = () => {
             <Typography variant="h6" component="div">
               Уроки
             </Typography>
-            <IconButton
-              size="large"
-              edge="end"
-              color="inherit"
-              aria-label="menu"
-            >
-              <MenuIcon />
-            </IconButton>
           </Stack>
         </Container>
       </Toolbar>
